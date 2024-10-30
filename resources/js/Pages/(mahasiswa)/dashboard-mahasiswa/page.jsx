a8a43f04a83e979d8afe067aadd94fa2eb2f613b
matahariann <dzusm@students.undip.ac.id>
2024-10-30 11:44:53 +0700

    Update: Page Dashboard Mahasiswa

--- resources/js/Pages/(mahasiswa)/dashboard-mahasiswa/page.jsx
+++ resources/js/Pages/(mahasiswa)/dashboard-mahasiswa/page.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { usePage } from "@inertiajs/inertia-react";
 import MahasiswaLayout from "../../../Layouts/MahasiswaLayout";
+import { Icon } from "@iconify/react";
 
 const DashboardMahasiswa = () => {
     const { props } = usePage();
@@ -13,7 +14,104 @@ const DashboardMahasiswa = () => {
 
     return (
         <MahasiswaLayout mahasiswa={mahasiswa}>
-            <h2>Home</h2>
+            <main className="flex-1 max-h-full">
+                <div className="flex flex-col items-start justify-between pb-6 space-y-4 border-b lg:items-center lg:space-y-0 lg:flex-row">
+                    <h1 className="text-2xl font-semibold whitespace-nowrap text-black">
+                        Dashboard
+                    </h1>
+                </div>
+                <div className="grid grid-cols-1 gap-5 mt-6">
+                    <div className="p-3 transition-shadow border rounded-lg shadow-sm hover:shadow-lg bg-gray-100">
+                        <div className="justify-between px-4 border rounded-lg shadow-lg bg-white">
+                            <div className="flex justify-center items-center mt-3 mb-3">
+                                <Icon
+                                    icon="fluent:building-bank-20-filled"
+                                    width="30"
+                                    height="30"
+                                />
+                                <h2 className="text-xl font-semibold whitespace-nowrap text-center ml-2">
+                                    Status Akademik
+                                </h2>
+                            </div>
+                            <div className="border-b w-full mt-2"></div>
+                            <div className="flex justify-center items-center mt-3 mb-1">
+                                <Icon
+                                    icon="mdi:person"
+                                    width="20"
+                                    height="20"
+                                />
+                                <span className="block text-lg text-gray-700">
+                                    <strong>Dosen wali:</strong> Dr. Sutikno,
+                                    S.T., M.Cs.
+                                </span>
+                            </div>
+                            <div className="flex justify-center items-center mb-3">
+                                <span className="block text-lg text-gray-700">
+                                    <strong>(NIP:</strong> 197905242009121003)
+                                </span>
+                            </div>
+                            <div className="grid grid-cols-3 text-center mb-3">
+                                <div className="border-r border-gray-300">
+                                    <p className="text-gray-500 text-md">
+                                        Semester Akademik Sekarang
+                                    </p>
+                                    <p className="font-semibold text-xl">
+                                        2024/2025 Ganjil
+                                    </p>
+                                </div>
+                                <div className="border-r border-gray-300">
+                                    <p className="text-gray-500 text-md">
+                                        Semester Studi
+                                    </p>
+                                    <p className="font-semibold text-xl">5</p>
+                                </div>
+                                <div>
+                                    <p className="text-gray-500 text-md">
+                                        Status Akademik
+                                    </p>
+                                    <span className="inline-block px-3 py-1 mt-1 text-sm font-semibold text-white bg-green-500 rounded-md">
+                                        AKTIF
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 gap-5 mt-4">
+                    <div className="p-3 transition-shadow border rounded-lg shadow-sm hover:shadow-lg bg-gray-100">
+                        <div className="justify-between px-4 border rounded-lg shadow-lg bg-white">
+                            <div className="flex justify-center items-center mt-3 mb-3">
+                                <Icon
+                                    icon="stash:trophy-solid"
+                                    width="30"
+                                    height="30"
+                                />
+                                <h2 className="text-xl font-semibold whitespace-nowrap text-center ml-2">
+                                    Prestasi Akademik
+                                </h2>
+                            </div>
+                            <div className="border-b w-full mt-4"></div>
+                            <div className="grid grid-cols-2 text-center mb-3 mt-3">
+                                <div className="border-r border-gray-300">
+                                    <p className="text-gray-500 text-lg">
+                                        IPk
+                                    </p>
+                                    <p className="font-semibold text-xl">
+                                        3.79
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="text-gray-500 text-lg">
+                                        SKSk
+                                    </p>
+                                    <p className="font-semibold text-xl">87</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </MahasiswaLayout>
     );
 };
